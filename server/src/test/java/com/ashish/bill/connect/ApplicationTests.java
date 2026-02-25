19db3119f65ceed5e422d34dc18ac857abbc9999
Ashish jha <ajha5645@gmail.com>
2026-02-25 16:56:36 +0530

    docs: Add comprehensive API specification for the Bill Connect API.

---
+++ server/src/test/java/com/ashish/bill/connect/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashish.bill.connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
